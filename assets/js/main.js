00fd73c61759758c8905d5eb393fd28f570e32b9
tujade <jade.mundim@gmail.com>
2021-11-23 22:08:12 -0300

    Srol UP  HTML, CSS and JS :white_check_mark:

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,13 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
